bb553cd5dc51f09934b9131848cf1d63e5e637e2
Cole2's World GitHub <Cole2's World GitHub>
2012-06-24 14:56:10 -0700

    Remove /cb lban

--- src/com/cole2sworld/ColeBans/LogManager.java
+++ src/com/cole2sworld/ColeBans/LogManager.java
@@ -1,53 +1,82 @@
 package com.cole2sworld.ColeBans;
 
-import java.util.ArrayList;
-import java.util.List;
+import static com.cole2sworld.ColeBans.handlers.MySQLBanHandler.addSlashes;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-import static com.cole2sworld.ColeBans.handlers.MySQLBanHandler.addSlashes;
+import com.unibia.simplemysql.SimpleMySQL;
 
 import com.cole2sworld.ColeBans.framework.LogEntry;
-import com.unibia.simplemysql.SimpleMySQL;
 
 /**
  * Manages logging of actions.
+ * 
  * @author cole2
- *
+ * 
  */
 public class LogManager {
 	public static enum Type {
-		BAN, UNBAN, TEMPBAN, KICK, SWITCH, LOCAL_BAN, OTHER, UNKNOWN, BANHAMMER_BAN, BANHAMMER_KICK;
-		public static Type forOrdinal(int ordinal) {
-			if (ordinal < 0 || ordinal >= values().length) return UNKNOWN;
+		BAN ,
+		UNBAN ,
+		TEMPBAN ,
+		KICK ,
+		SWITCH ,
+		LOCAL_BAN ,
+		OTHER ,
+		UNKNOWN ,
+		BANHAMMER_BAN ,
+		BANHAMMER_KICK;
+		public static Type forOrdinal(final int ordinal) {
+			if ((ordinal < 0) || (ordinal >= values().length)) return UNKNOWN;
 			return values()[ordinal];
 		}
 	}
-	private static String tablePrefix;
-	private static SimpleMySQL sql;
-	private static String tbl;
-	private static boolean initialized = false;
+	
+	private static String		tablePrefix;
+	private static SimpleMySQL	sql;
+	private static String		tbl;
+	private static boolean		initialized	= false;
+	
 	/**
-	 * Initialize LogManager - connect to MySQL, set up variables, etc.
+	 * Add an entry to the log, using the current time.<br/>
+	 * <i>Ban handlers should <b>never</b> call this, unless logging a 'UNKNOWN'
+	 * or 'OTHER' action. All the command classes will log for you.</i>
+	 * 
+	 * @param type
+	 *            Type of action being logged (LogManager.Type)
+	 * @param admin
+	 *            Admin that took the action
+	 * @param victim
+	 *            Victim of the action
 	 */
-	public static void initialize() {
-		if (initialized) return;
-		if (!Main.instance.banHandler.getTruncatedName().equals("mysql")) {
-			Main.LOG.warning(Main.PREFIX+"[ActionLogger] Could not initalize - current ban handler is not MySQL.");
-			return;
+	public static void addEntry(final Type type, final String admin, final String victim) {
+		verify();
+		final PreparedStatement stmt = sql.prepare("INSERT INTO " + tbl
+				+ " (type, admin, victim, time) VALUES (?, ?, ?, ?);");
+		try {
+			Main.debug("Logging " + admin.toLowerCase() + " doing something to "
+					+ victim.toLowerCase());
+			stmt.setInt(1, type.ordinal());
+			stmt.setString(2, admin.toLowerCase());
+			stmt.setString(3, victim.toLowerCase());
+			stmt.setLong(4, System.currentTimeMillis());
+			stmt.execute();
+		} catch (final SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+			} catch (final SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		Map<String, String> data = Main.getBanHandlerInitArgs();
-		sql = new SimpleMySQL();
-		sql.enableReconnect();
-		sql.setReconnectNumRetry(25);
-		sql.connect(data.get("host"), data.get("username"), data.get("password"));
-		sql.use(data.get("db"));
-		tablePrefix = data.get("prefix");
-		tbl = tablePrefix+"log";
-		initialized = true;
 	}
+	
 	/**
 	 * De-initialize LogManager - just disconnects from MySQL.
 	 */
@@ -57,185 +86,182 @@ public class LogManager {
 		sql = null;
 		initialized = false;
 	}
-	/**
-	 * Add an entry to the log, using the current time.<br/>
-	 * <i>Ban handlers should <b>never</b> call this, unless logging a 'UNKNOWN' or 'OTHER' action. All the command classes will log for you.</i>
-	 * @param type Type of action being logged (LogManager.Type)
-	 * @param admin Admin that took the action
-	 * @param victim Victim of the action
-	 */
-	public static void addEntry(Type type, String admin, String victim) {
-		verify();
-		PreparedStatement stmt = sql.prepare("INSERT INTO "+tbl+" (type, admin, victim, time) VALUES (?, ?, ?, ?);");
-		try {
-			Main.debug("Logging "+admin.toLowerCase()+" doing something to "+victim.toLowerCase());
-			stmt.setInt(1, type.ordinal());
-			stmt.setString(2, admin.toLowerCase());
-			stmt.setString(3, victim.toLowerCase());
-			stmt.setLong(4, System.currentTimeMillis());
-			stmt.execute();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				stmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+	
 	/**
 	 * Get all actions limiting to <code>limit</code>
-	 * @param limit Amount of entries to limit to, or -1 to get all entires
+	 * 
+	 * @param limit
+	 *            Amount of entries to limit to, or -1 to get all entires
 	 * @return All results
 	 */
-	public static List<LogEntry> getAll(int limit) {
+	public static List<LogEntry> getAll(final int limit) {
 		verify();
-		ArrayList<LogEntry> entries = new ArrayList<LogEntry>();
-		PreparedStatement stmt = sql.prepare("SELECT * FROM "+tbl+" ORDER BY time DESC "+(limit == -1 ? ";" : "LIMIT "+limit+";"));
+		final ArrayList<LogEntry> entries = new ArrayList<LogEntry>();
+		final PreparedStatement stmt = sql.prepare("SELECT * FROM " + tbl + " ORDER BY time ASC "
+				+ (limit == -1 ? ";" : "LIMIT " + limit + ";"));
 		try {
-			ResultSet result = stmt.executeQuery();
+			final ResultSet result = stmt.executeQuery();
 			for (; result.next();) {
-				entries.add(new LogEntry(
-						Type.forOrdinal(result.getInt("type")),
-						result.getString("admin"),
-						result.getString("victim"),
-						result.getLong("time")
-						));
+				entries.add(new LogEntry(Type.forOrdinal(result.getInt("type")), result
+						.getString("admin"), result.getString("victim"), result.getLong("time")));
 			}
 			return entries;
-		} catch (SQLException e) {
+		} catch (final SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				stmt.close();
-			} catch (SQLException e) {
+			} catch (final SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		return null;
 	}
+	
 	/**
 	 * Get all actions made by a specific admin
-	 * @param admin The admin to lookup
+	 * 
+	 * @param admin
+	 *            The admin to lookup
 	 * @return All results
 	 */
-	public static List<LogEntry> getBy(String admin) {
+	public static List<LogEntry> getBy(final String admin) {
 		verify();
-		ArrayList<LogEntry> entries = new ArrayList<LogEntry>();
-		PreparedStatement stmt = sql.prepare("SELECT * FROM "+tbl+" WHERE admin=?");
+		final ArrayList<LogEntry> entries = new ArrayList<LogEntry>();
+		final PreparedStatement stmt = sql.prepare("SELECT * FROM " + tbl + " WHERE admin=?");
 		try {
 			stmt.setString(1, admin.toLowerCase());
-			ResultSet result = stmt.executeQuery();
+			final ResultSet result = stmt.executeQuery();
 			for (; result.next();) {
-				entries.add(new LogEntry(
-						Type.forOrdinal(result.getInt("type")),
-						result.getString("admin"),
-						result.getString("victim"),
-						result.getLong("time")
-						));
+				entries.add(new LogEntry(Type.forOrdinal(result.getInt("type")), result
+						.getString("admin"), result.getString("victim"), result.getLong("time")));
 			}
 			return entries;
-		} catch (SQLException e) {
+		} catch (final SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				stmt.close();
-			} catch (SQLException e) {
+			} catch (final SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		return null;
 	}
-	/**
-	 * Get all actions made on a specific victim
-	 * @param victim The victim to lookup
-	 * @return All results
-	 */
-	public static List<LogEntry> getTo(String victim) {
-		verify();
-		ArrayList<LogEntry> entries = new ArrayList<LogEntry>();
-		PreparedStatement stmt = sql.prepare("SELECT * FROM "+tbl+" WHERE victim=?");
-		try {
-			stmt.setString(1, victim.toLowerCase());
-			ResultSet result = stmt.executeQuery();
-			for (; result.next();) {
-				entries.add(new LogEntry(
-						Type.forOrdinal(result.getInt("type")),
-						result.getString("admin"),
-						result.getString("victim"),
-						result.getLong("time")
-						));
-			}
-			return entries;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				stmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return null;
-	}
+	
 	/**
 	 * Get all actions made on a specific victim, by a specific admin
-	 * @param admin Admin to lookup
-	 * @param victim Victim to lookup
+	 * 
+	 * @param admin
+	 *            Admin to lookup
+	 * @param victim
+	 *            Victim to lookup
 	 * @return All results
 	 */
-	public static List<LogEntry> getByOn(String admin, String victim) {
+	public static List<LogEntry> getByOn(final String admin, final String victim) {
 		verify();
-		ArrayList<LogEntry> entries = new ArrayList<LogEntry>();
-		String st = "SELECT * FROM "+tbl+" WHERE ("+(admin.equals("*") ? "" : "admin='"+addSlashes(admin.toLowerCase())+"'")+(victim.equals("*") || admin.equals("*") ? "" : ", ")+(victim.equals("*") ? "" : "victim='"+addSlashes(victim.toLowerCase())+"'")+");";
+		final ArrayList<LogEntry> entries = new ArrayList<LogEntry>();
+		final String st = "SELECT * FROM " + tbl + " WHERE ("
+				+ (admin.equals("*") ? "" : "admin='" + addSlashes(admin.toLowerCase()) + "'")
+				+ (victim.equals("*") || admin.equals("*") ? "" : ", ")
+				+ (victim.equals("*") ? "" : "victim='" + addSlashes(victim.toLowerCase()) + "'")
+				+ ");";
 		Main.debug(st);
-		PreparedStatement stmt = sql.prepare(st);
+		final PreparedStatement stmt = sql.prepare(st);
 		try {
-			ResultSet result = stmt.executeQuery();
+			final ResultSet result = stmt.executeQuery();
 			for (; result.next();) {
-				entries.add(new LogEntry(
-						Type.forOrdinal(result.getInt("type")),
-						result.getString("admin"),
-						result.getString("victim"),
-						result.getLong("time")
-						));
+				entries.add(new LogEntry(Type.forOrdinal(result.getInt("type")), result
+						.getString("admin"), result.getString("victim"), result.getLong("time")));
 			}
 			return entries;
-		} catch (SQLException e) {
+		} catch (final SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				stmt.close();
-			} catch (SQLException e) {
+			} catch (final SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		return null;
 	}
+	
+	/**
+	 * Get all actions made on a specific victim
+	 * 
+	 * @param victim
+	 *            The victim to lookup
+	 * @return All results
+	 */
+	public static List<LogEntry> getTo(final String victim) {
+		verify();
+		final ArrayList<LogEntry> entries = new ArrayList<LogEntry>();
+		final PreparedStatement stmt = sql.prepare("SELECT * FROM " + tbl + " WHERE victim=?");
+		try {
+			stmt.setString(1, victim.toLowerCase());
+			final ResultSet result = stmt.executeQuery();
+			for (; result.next();) {
+				entries.add(new LogEntry(Type.forOrdinal(result.getInt("type")), result
+						.getString("admin"), result.getString("victim"), result.getLong("time")));
+			}
+			return entries;
+		} catch (final SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+			} catch (final SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Initialize LogManager - connect to MySQL, set up variables, etc.
+	 */
+	public static void initialize() {
+		if (initialized) return;
+		if (!Main.instance.banHandler.getTruncatedName().equals("mysql")) {
+			Main.LOG.warning(Main.PREFIX
+					+ "[ActionLogger] Could not initalize - current ban handler is not MySQL.");
+			return;
+		}
+		final Map<String, String> data = Main.getBanHandlerInitArgs();
+		sql = new SimpleMySQL();
+		sql.enableReconnect();
+		sql.setReconnectNumRetry(25);
+		sql.connect(data.get("host"), data.get("username"), data.get("password"));
+		sql.use(data.get("db"));
+		tablePrefix = data.get("prefix");
+		tbl = tablePrefix + "log";
+		initialized = true;
+	}
+	
 	/**
 	 * Cut a list of LogEntrys to a certain time
-	 * @param timeMillis Time to cut to
+	 * 
+	 * @param timeMillis
+	 *            Time to cut to
 	 * @return Modified list
 	 */
-	public static List<LogEntry> since(long timeMillis, List<LogEntry> oldlist) {
-		ArrayList<LogEntry> list = new ArrayList<LogEntry>();
-		for (LogEntry entry : oldlist) {
+	public static List<LogEntry> since(final long timeMillis, final List<LogEntry> oldlist) {
+		final ArrayList<LogEntry> list = new ArrayList<LogEntry>();
+		for (final LogEntry entry : oldlist) {
 			if (entry.getTime() >= timeMillis) {
 				list.add(entry);
 			}
 		}
 		return list;
 	}
+	
 	private static void verify() {
 		initialize();
 		if (!sql.checkTable(tbl)) {
-			sql.query("CREATE  TABLE "+tbl+" ("+
-					"`type` INT UNSIGNED NOT NULL ,"+
-					"`admin` VARCHAR(45) NULL ,"+
-					"`victim` VARCHAR(45) NULL ,"+
-					"`time` BIGINT PK UNSIGNED NULL ,"+
-					"INDEX `main` (`time` ASC) );");
+			sql.query("CREATE  TABLE " + tbl + " (" + "`type` INT UNSIGNED NOT NULL ,"
+					+ "`admin` VARCHAR(45) NULL ," + "`victim` VARCHAR(45) NULL ,"
+					+ "`time` BIGINT PK UNSIGNED NULL ," + "INDEX `main` (`time` ASC) );");
 		}
 	}
 }
